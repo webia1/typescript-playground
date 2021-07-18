cc71619ea555db06ef3cc9da286bae8a4efe6ee1
Erkan Buelbuel <erkan.buelbuel@bit.admin.ch>
2021-07-18 16:24:49 +0200

    Observer

--- src/rxjs/Observer/simple.ts
+++ src/rxjs/Observer/simple.ts
@@ -3,15 +3,10 @@ import { Observable } from 'rxjs';
 const observable$ = new Observable<string>((subscriber) => {
   subscriber.next('Alice');
   subscriber.next('Ben');
-  subscriber.error('Error happened!');
   subscriber.complete();
-  return () => {
-    console.log('Tear Down Process');
-  };
 });
 
 observable$.subscribe({
   next: (value: any) => console.log(value),
-  error: (error: any) => console.log(error),
   complete: () => console.log('Completed!'),
 });
