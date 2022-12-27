2f100464bb53cce373af0efb9330870aaaea8158
webia1 <webia1.github@gmail.com>
2022-12-27 09:14:21 +0100

    nodemon.json update

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('Hi');
+const timeRegExp = /([1-9]|1[0-2]):([0-5][0-9])([ap]m)?/g;
+let timeResult;
+timeResult = 'Lunch at 11:45 or 12:45?'.match(timeRegExp);
+console.log(timeResult);
+
+/** outpus
+[ '11:45', '12:45' ]
+*/
