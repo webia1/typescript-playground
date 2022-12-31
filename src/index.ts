0df871e81d2bccefeaf4eddab0ebe327475bdd5c
webia1 <webia1.github@gmail.com>
2022-12-31 10:05:37 +0100

    new T test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-const timeRegExp = /([1-9]|1[0-2]):([0-5][0-9])([ap]m)?/g;
-let timeResult;
-timeResult = 'Lunch at 11:45 or 12:45?'.match(timeRegExp);
-console.log(timeResult);
+// const timeRegExp = /([1-9]|1[0-2]):([0-5][0-9])([ap]m)?/g;
+// let timeResult;
+// timeResult = 'Lunch at 11:45 or 12:45?'.match(timeRegExp);
+// console.log(timeResult);
 
 /** outpus
 [ '11:45', '12:45' ]
