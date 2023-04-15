54fd9c03e0aca737901ef19f561e6e82e74d4f41
webia1 <webia1.github@gmail.com>
2023-04-15 11:42:41 +0200

    JS Map

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,74 @@
-// create 100KB empty PDF file
-const { PDFDocument } = require('pdf-lib');
-const fs = require('fs');
-const path = require('path');
+const planets = [
+  { id: 1, name: 'Merkur' },
+  { id: 2, name: 'Venus' },
+  { id: 3, name: 'Earth' },
+  { id: 2, name: 'Venus' }, // duplicate ID
+  { id: 4, name: 'Mars' },
+];
 
-async function createEmptyPdf() {
-  const targetSize = 1 * 1024 * 1024;
-  const folderPath = './PDF/100KB';
-  const name = '1MB_';
-  const count = 60;
+const uniquePlanetsMap = new Map();
 
-  if (!fs.existsSync(folderPath)) {
-    fs.mkdirSync(folderPath);
-  }
+/**
+ * Usage: someMap.set(key, value)
+ */
 
-  for (let i = 1; i <= count; i++) {
-    const pdfDoc = await PDFDocument.create();
-    const pdfBytes = await pdfDoc.save();
-    const padding = ' '.repeat(targetSize - pdfBytes.length);
-    const outputBytes = Buffer.concat([
-      pdfBytes,
-      Buffer.from(padding),
-    ]);
-    const fileName = `${name}${i}.pdf`;
-    const filePath = path.join(folderPath, fileName);
-    fs.writeFileSync(filePath, outputBytes);
-    console.log(`Created file: ${filePath}`);
-  }
+planets.forEach((planet) => {
+  /**
+   * Very important: The key of the map must be a unique value.
+   * That means: We use the 'id' of the planet as key.
+   * Map.set() will overwrite the value if the key already exists.
+   */
+
+  uniquePlanetsMap.set(planet.id, planet);
+});
+
+console.log(uniquePlanetsMap.size); // 4, since the duplicate was removed
+console.log(uniquePlanetsMap.has(2)); // true
+
+const merkur = uniquePlanetsMap.get(1);
+console.log(merkur); // { id: 1, name: 'Merkur' }
+
+console.log(uniquePlanetsMap);
+
+/* Output:
+Map(4) {
+  1 => { id: 1, name: 'Merkur' },
+  2 => { id: 2, name: 'Venus' },
+  3 => { id: 3, name: 'Earth' },
+  4 => { id: 4, name: 'Mars' }
 }
+*/
 
-createEmptyPdf();
+console.log(Array.from(uniquePlanetsMap));
+
+/* Output:
+[
+  [ 1, { id: 1, name: 'Merkur' } ],
+  [ 2, { id: 2, name: 'Venus' } ],
+  [ 3, { id: 3, name: 'Earth' } ],
+  [ 4, { id: 4, name: 'Mars' } ]
+]
+*/
+
+console.log(Array.from(uniquePlanetsMap.values()));
+
+/* Output:
+[
+  { id: 1, name: 'Merkur' },
+  { id: 2, name: 'Venus' },
+  { id: 3, name: 'Earth' },
+  { id: 4, name: 'Mars' }
+]
+*/
+
+uniquePlanetsMap.set(5, { id: 5, name: 'Jupiter' });
+
+/**
+ * ATTENTION: The first parameter key has to be equal
+ * to the 'id' of the Object(=Value).
+ * If you use a different value:
+ * - the Map will not be updated or
+ * - the Map will have doubled entries
+ */
+
+console.log(uniquePlanetsMap);
